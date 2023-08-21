bb99ba2116059f5ac441fcc9dfbb7ca2e51c1cc0
Hakan Bilgin <hbi99@hotmail.com>
2023-08-21 20:00:53 +0300

    puzzle parts ui

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,17 @@
 
+@import "modules/maps.js";
+
+
+let blank = "jws,pwe,jwse,pwe,jwse,pwe,jse,pns,b,pns,b,pns,b,pns,jnws,pwe,jnwse,pwe,jnwse,pwe,jnse,pns,b,pns,b,pns,b,pns,jnws,pwe,jnwse,pwe,jnwse,pwe,jnse,pns,b,pns,b,pns,b,pns,jnw;E,pwe,jnwe,pwe,jnwe,pwe,jne";
+let all = "jws;X,pwe,jwse;X,bwe,jwse,pwe,jse;X,pns,b,pns,b,pns,b,pns,jnws;X,bwe,jnwse,bwe,jnwse,pwe,jnse,pns,b,pns,b,bns,b,pns,jnws,bwe,jnwse;E,bwe,jnwse,pwe,jnse;X,pns,b,pns,b,pns,b,pns,jnw;X,bwe,jnwe;X,bwe,jnwe,pwe,jne;X";
+
+
 const windmill = {
 	init() {
 		// fast references
 		this.content = window.find("content");
+
+		Maps.draw(blank);
 	},
 	dispatch(event) {
 		switch (event.type) {
---
+++ src/js/modules/maps.js
@@ -0,0 +1,36 @@
+
+let lexer = {
+	"jnwse": "junc-nwse",
+	"jnse": "junc-nse",
+	"jnws": "junc-nws",
+	"jwse": "junc-wse",
+	"jnwe": "junc-nwe",
+	"jse": "junc-se",
+	"jws": "junc-ws",
+	"jne": "junc-ne",
+	"jnw": "junc-nw",
+	"pwe": "path-we",
+	"pns": "path-ns",
+	"bwe": "break-we",
+	"bns": "break-ns",
+	"b": "box",
+	"E": "entry",
+	"X": "exit",
+	"Xn": "exit n",
+	"Xw": "exit w",
+	"Xs": "exit s",
+	"Xe": "exit e",
+};
+
+let Maps = {
+	draw(map) {
+		let out = [];
+
+		map.split(",").map(cell => {
+			let names = cell.split(";").map(p => lexer[p]).join(" ");
+			out.push(`<span class="${names}"></span>`);
+		});
+
+		window.find(".puzzle").html(out.join(""));
+	}
+};
