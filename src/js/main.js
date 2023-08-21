d2c73ca4d94c4cc5ceb6eeda2b1cda4096529c97
Hakan Bilgin <hbi99@hotmail.com>
2023-08-21 23:50:49 +0300

    added snake

--- src/js/main.js
+++ src/js/main.js
@@ -8,8 +8,16 @@ const windmill = {
 		// fast references
 		this.content = window.find("content");
 
-		Maps.draw({ name: "plus-x", el: window.find(".puzzle.p1") });
+		Maps.draw({ name: "plus", el: window.find(".puzzle.p1") });
 		Maps.draw({ name: "exits", el: window.find(".puzzle.p2") });
+
+		let snake = [];
+		snake.push(`<circle cx="91" cy="231" r="21"/>`);
+		snake.push(`<line x1="91" y1="231" x2="91" y2="21"/>`);
+		snake.push(`<line x1="91" y1="21" x2="131" y2="21"/>`);
+
+		window.find(".puzzle.p1")
+			.append(`<svg class="snake" viewBox="0 0 252 252">${snake.join("")}</svg>`);
 	},
 	dispatch(event) {
 		switch (event.type) {
