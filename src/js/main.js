044b41617fa34109ea9a7c46db6f76d7f8b7b3ff
Hakan Bilgin <hbi99@hotmail.com>
2024-06-13 00:39:47 +0200

    clean up

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,13 @@
 
-@import "modules/sample.js";
-@import "modules/levels.js";
-@import "modules/maps.js";
-@import "modules/particles.js";
-@import "modules/snake.js";
-
-
-let Input = {
-	keys: "UP RIGHT DOWN LEFt".split(" "),
-	key: {},
-};
+@import "modules/test.js";
 
 
 const witness = {
 	init() {
-		// fast references
-		this.content = window.find("content");
-		this.coords = window.find(`.tools input[name="coords"]`);
-		// init sub objects
-		Particles.init();
-		Snake.init();
-
-		// Maps.draw({ name: "level1a-H", el: window.find(".puzzle.p1").addClass("show") });
-		// Maps.draw({ name: "level1b-H", el: window.find(".puzzle.p2").addClass("show") });
-		// Maps.draw({ name: "level1a-V", el: window.find(".puzzle.p3").addClass("show") });
-		// Maps.draw({ name: "level1b-V", el: window.find(".puzzle.p4").addClass("show") });
-
-		let el = window.find(".puzzle.p1");
-		// Maps.draw({ gen: "4x4", el });
-		Maps.draw({ name: "large", el });
 		
-		// PGN
-		// let value = Maps.pgn(el);
-		// console.log( value );
-		// console.log( lvl["simple-symmetry"] );
-
-
-		// let path = "0,3,N;0,2;1,2;1,1;3,1;3,0;4,0;4,1;5,1;5,2;3,2;3,3";
-		// Snake.draw({ path, el: window.find(".puzzle.p1") });
-		// Particles.setup({ path, el: window.find(".puzzle.p1") });
-
-		// Maps.draw({ name: "exits", el: window.find(".puzzle.p2") });
-		// Maps.draw({ gen: "4x3", el: window.find(".puzzle.p2") });
-
-		// let ev = {
-		// 	el: window.find(".puzzle span.entry:nth(1)"),
-		// 	clientX: 267,
-		// 	clientY: 525,
-		// };
-		// Snake.start(ev);
-
-		// this.content.find(".puzzle span.entry:nth(0)").trigger("click");
-		// Snake.getLimits([0,210]);
+		// DEV-ONLY-START
+		Test.init(this);
+		// DEV-ONLY-END
 	},
 	dispatch(event) {
 		let Self = witness,
@@ -60,35 +16,7 @@ const witness = {
 			// system events
 			case "window.init":
 				break;
-			case "window.keyup":
-				// reset all keys
-				Input.keys.map(k => Input.key[k] = false);
-				Input.dir = false;
-				break;
-			case "window.keystroke":
-				value = event.char.toUpperCase();
-				Input.dir = Input.keys.indexOf(value);
-				Input.key[value] = true;
-				if (Snake.pos) Snake.move({ dir: Input.dir });
-				break;
 			// custom events
-			case "start-snake":
-				Snake.dispatch(event);
-				break;
-			case "move-range":
-				// Particles.update(event.value / 100);;
-				break;
-			case "output-pgn":
-				value = Maps.pgn(event.origin.el);
-				console.log(value);
-				break;
-			case "output-blank-pgn":
-				value = Maps.generate(event.arg);
-				console.log(value);
-				break;
-			case "draw-level":
-				Maps.draw({ name: event.arg, el: window.find(".puzzle") });
-				break;
 			case "open-help":
 				karaqu.shell("fs -u '~/help/index.md'");
 				break;
---
+++ src/js/modules/test.js
@@ -0,0 +1,7 @@
+
+let Test = {
+	init(APP) {
+		
+	}
+};
+
